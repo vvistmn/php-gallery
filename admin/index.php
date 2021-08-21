ffd60b73d301fef9832bd158045190a7521ae831
Vladimir Istomin <verdant24@yandex.ru>
2021-08-21 13:29:07 +0300

    Первый коммит

---
+++ admin/index.php
@@ -0,0 +1,19 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+            <?php include 'includes/top_nav.php'?>
+
+            <?php include 'includes/side_nav.php'?>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <?php include 'includes/admin_content.php'?>
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
