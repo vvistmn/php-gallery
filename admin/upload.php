756a3a751cffd6eb18ba1f4972f8a6775b5d9c02
Vladimir Istomin <verdant24@yandex.ru>
2021-08-22 07:58:44 +0300

    Добавили новый маршруты

---
+++ admin/upload.php
@@ -0,0 +1,41 @@
+<?php include("includes/header.php"); ?>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+        <?php include 'includes/top_nav.php'?>
+
+        <?php include 'includes/side_nav.php'?>
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Page Upload
+                        <small>Upload</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li>
+                            <i class="fa fa-dashboard"></i>  <a href="./">Dashboard</a>
+                        </li>
+                        <li class="active">
+                            <i class="fa fa-file"></i>Upload
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
